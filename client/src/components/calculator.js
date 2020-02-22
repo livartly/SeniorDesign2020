ca645d5488d16b9a7dd72888c7b72ab07c506b78
codemanpete <rhoxwarrior@gmail.com>
2020-02-22 00:48:32 -0600

    Add legend to TruthTableBuilder

--- client/src/components/calculator.js
+++ client/src/components/calculator.js
@@ -33,7 +33,13 @@ class Calculator extends React.Component {
       <Table striped bordered>
         <thead>
           <tr>
-            {outArray[0].slice(1, outArray[0].length - 1).split('|').map((item, i) => <th key={i}>{item.replace(/&#124;/g, '|')}</th>)}
+            {outArray[0]
+              .slice(1, outArray[0].length - 1)
+              .split('|')
+              .map((item, i) =>
+                <th key={i}>{item.replace(/&#124;/g, '|')}</th>
+              )
+            }
           </tr>
         </thead>
         <tbody>
@@ -46,7 +52,13 @@ class Calculator extends React.Component {
   makeRow(rowArray, i) {
     return (
       <tr key={i}>
-        {rowArray.slice(1, rowArray.length - 1).split('|').map((item, j) => <td key={j}>{item}</td>)}
+        {rowArray
+          .slice(1, rowArray.length - 1)
+          .split('|')
+          .map((item, j) =>
+            <td key={j}>{item}</td>
+          )
+        }
       </tr>
     );
   }
@@ -56,11 +68,37 @@ class Calculator extends React.Component {
       <Table striped border>
         <thead>
           <tr>
-
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Example</th>
           </tr>
         </thead>
         <tbody>
-
+          <tr>
+            <td>~</td>
+            <td>Negation</td>
+            <td>~P ("not P")</td>
+          </tr>
+          <tr>
+            <td>&amp;</td>
+            <td>Conjunction</td>
+            <td>P &amp; Q ("P and Q")</td>
+          </tr>
+          <tr>
+            <td>||</td>
+            <td>Disjunction</td>
+            <td>P || Q ("P or Q")</td>
+          </tr>
+          <tr>
+            <td>-></td>
+            <td>Implication</td>
+            <td>P -> Q ("If P, then Q")</td>
+          </tr>
+          <tr>
+            <td>&lt;-&gt;</td>
+            <td>Equivalence</td>
+            <td>P &lt;-&gt; Q ("P if and only if Q")</td>
+          </tr>
         </tbody>
       </Table>
     );
@@ -70,7 +108,9 @@ class Calculator extends React.Component {
     console.log("out is " + this.state.out);
     return (
       <div className="box">
-        <h3>Truth Table Builder</h3>
+        <h2>Truth Table Builder</h2>
+        <h3>Legend</h3>
+        {this.makeLegend()}
         <p>Type a wff in the box</p>
         <textarea value={this.state.wff} onChange={this.updateWff}></textarea>
         <br />
