5eac671fcc22c3d9562b1a6f913db0891a3d8a2a
Liv Weaver <livweaverdesign@gmail.com>
2020-03-18 16:16:05 -0500

    About page update

--- client/src/components/about/about.js
+++ client/src/components/about/about.js
@@ -10,7 +10,7 @@ class About extends Component {
               <div className="leftimage">
                 <img src="images/question.png" width="100%" alt="Answers Image" />
                 <h1>Get Answers!</h1>
-                <h2>Enter Discrete Mathematics problems and receive instant answers</h2>
+                <h5>Enter Discrete Mathematics problems and receive instant answers</h5>
               </div>
               <a className="button button-primary" href="">ENTER</a>
             </div>
@@ -22,7 +22,7 @@ class About extends Component {
                 <img src="images/descriptor.png" width="100%" alt="Resources Image" />
               </div>
               <h1>Access Resources</h1>
-              <h2>Visit our list of additional resources hand picked for struggling students </h2>
+              <h5>Visit our list of additional resources hand picked for struggling students </h5>
               <a className="button button-primary" href="">VISIT</a>
             </div>
           </div>
@@ -32,13 +32,11 @@ class About extends Component {
               <div className="rightimage">
                 <img src="images/project-management.png" width="100%" alt="Admin Image" />
                 <h1>Admin Assistance</h1>
-                <h2>Message us for administrative assistance with specific inquiries and issues.</h2>
+                <h5>Message us for administrative assistance with specific inquiries and issues.</h5>
               </div>
               <a className="button button-primary" href="">CONTACT</a>
             </div>
           </div>
-
-          <div className="contactme"><a href=""><b>CONTACT</b></a></div>
         </div>
       </div>
     );
