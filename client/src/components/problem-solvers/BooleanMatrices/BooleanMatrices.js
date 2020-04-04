8292a067080d792d82bf02700f26f2b0aef5dd0a
Lightrixian <wmgrbn@gmail.com>
2020-04-03 21:32:05 -0500

    Finished Linking Frontend and Backend
    Finished linking the frontend and backend for the Boolean Matrix logic. The logic has been tested against the homework problems in the book. There are two CSS format questions that I have for Liv in order to get my resizables to resize with the page without looking awkward.

--- client/src/components/problem-solvers/BooleanMatrices/BooleanMatrices.js
+++ client/src/components/problem-solvers/BooleanMatrices/BooleanMatrices.js
@@ -10,14 +10,19 @@ class BooleanMatrices extends React.Component
         this.state = {
             MatrixSize : 2,
             InputButtonsA: [[0,0],[0,0]],
-            InputButtonsB: [[0,0],[0,0]]
+            InputButtonsB: [[0,0],[0,0]],
+            AnswerAnd: undefined,
+            AnswerOr: undefined,
+            AnswerProduct: undefined,
+            AnswerReverseProduct: undefined
         };
 
         this.HandleClick = this.HandleClick.bind(this);
         this.updateMatrixSize = this.updateMatrixSize.bind(this);
         this.CreateInputButtons = this.CreateInputButtons.bind(this);
         this.UpdateButtonValues = this.UpdateButtonValues.bind(this);
-        this.createMatrixAForm = this.createMatrixAForm.bind(this);
+        this.createMatrixForm = this.createMatrixForm.bind(this);
+        this.displayAnswer = this.displayAnswer.bind(this);
     }
 
     updateMatrixSize(event)
@@ -30,26 +35,28 @@ class BooleanMatrices extends React.Component
     CreateInputButtons()
     {
         var InputButtonsMatrixA = [];
+        var InputButtonsMatrixB = [];
         
-
         for(var i = 0; i < this.state.MatrixSize; i++)
         {  
             InputButtonsMatrixA.push([])
+            InputButtonsMatrixB.push([])
             for(var j = 0; j < this.state.MatrixSize; j++)
             {
                 InputButtonsMatrixA[i].push(0); //defines row    row[i] = col[0,0,->n]
+                InputButtonsMatrixB[i].push(0);
             }
         }
 
-       // alert(this.state.InputButtonsA + "   " + InputButtonsMatrixA)
         this.setState(prevState => {
             return{
-                InputButtonsA : InputButtonsMatrixA 
+                InputButtonsA : InputButtonsMatrixA,
+                InputButtonsB : InputButtonsMatrixB
             };
         });
     }
 
-    UpdateButtonValues(rowIndex, colIndex){
+    UpdateButtonValues(rowIndex, colIndex, MatrixFlag){
         return e => {
             var nextVal = e.currentTarget.value;
             var newInputButton;
@@ -60,20 +67,34 @@ class BooleanMatrices extends React.Component
             }
             else newInputButton = 0
 
-            this.setState(prevState => {
+            if(MatrixFlag === 0)
+            {
+                this.setState(prevState => {
 
-                var InputButtonsMatrixA = [];
-                
-                
-                InputButtonsMatrixA = prevState.InputButtonsA;
-                
-               // alert(InputButtonsMatrixA[rowIndex][colIndex])
-                InputButtonsMatrixA[rowIndex][colIndex] = newInputButton;
-               // alert(InputButtonsMatrixA)
-                return{
-                    InputButtonsA: InputButtonsMatrixA
-                };
-            });
+                    var InputButtonsMatrixA = [];
+                    
+                    InputButtonsMatrixA = prevState.InputButtonsA;
+                    
+                    InputButtonsMatrixA[rowIndex][colIndex] = newInputButton;
+                    return{
+                        InputButtonsA: InputButtonsMatrixA
+                    };
+                });
+            }
+            else if(MatrixFlag === 1)
+            {
+                this.setState(prevState => {
+
+                    var InputButtonsMatrixB = [];
+                    
+                    InputButtonsMatrixB = prevState.InputButtonsB;
+                    
+                    InputButtonsMatrixB[rowIndex][colIndex] = newInputButton;
+                    return{
+                        InputButtonsB: InputButtonsMatrixB
+                    };
+                });
+            }
         };
     }
 
@@ -84,44 +105,76 @@ class BooleanMatrices extends React.Component
         ));
     }
 
-    createMatrixAForm()
+    createMatrixForm(MatrixFlag)
     {
-        return this.state.InputButtonsA.map((dataArrayRow, rowIndex) => {
-           return(
-               <div >
+        if(MatrixFlag == 0)
+        {
+            return this.state.InputButtonsA.map((dataArrayRow, rowIndex) => {
+            return(
+                <div >
                     <div key={rowIndex}>
-                            {this.renderRow(rowIndex)}
+                        {this.renderRow(rowIndex, MatrixFlag)}
                     </div>
                 </div>
-           );
-        });
-
-        
+            );
+            }); 
+        }
+        else if(MatrixFlag == 1)
+        {
+            return this.state.InputButtonsB.map((dataArrayRow, rowIndex) => {
+                return(
+                    <div >
+                        <div key={rowIndex}>
+                            {this.renderRow(rowIndex, MatrixFlag)}
+                        </div>
+                    </div>
+                );
+                }); 
+        }
     }
 
-    renderRow(rowIndex)
+    renderRow(rowIndex, MatrixFlag)
     {
-        return this.state.InputButtonsA[rowIndex].map((booleanNumber, colIndex) => {
-            return(
-                <div style={{display:"inline-block", marginBottom: -10}} >
-                   
-                    <div  key={colIndex}>
-                        <Button
-                        variant = "white"
-                        style = {{fontSize: "15px", width: 45, height: 45, borderColor: "lightgrey" , color: "black"}}
-                        value={this.state.InputButtonsA[rowIndex][colIndex]}
-                        onClick={this.UpdateButtonValues(rowIndex, colIndex)}
-                        > 
-                        {this.state.InputButtonsA[rowIndex][colIndex]}
-                        </Button>
-                        
-                    </div>
+        if(MatrixFlag == 0)
+        {
+            return this.state.InputButtonsA[rowIndex].map((booleanNumber, colIndex) => {
+                return(
+                    <div style={{display:"inline-block", marginBottom: -10}} >
                     
-                </div>
-                
-            );
-
-        });
+                        <div  key={colIndex}>
+                            <Button
+                            variant = "white"
+                            style = {{fontSize: "15px", width: 45, height: 45, borderColor: "lightgrey" , color: "black"}}
+                            value={this.state.InputButtonsA[rowIndex][colIndex]}
+                            onClick={this.UpdateButtonValues(rowIndex, colIndex, MatrixFlag)}
+                            > 
+                            {this.state.InputButtonsA[rowIndex][colIndex]}
+                            </Button>
+                        </div>
+                    </div>
+                );
+            });
+        }
+        else if(MatrixFlag == 1)
+        {
+            return this.state.InputButtonsB[rowIndex].map((booleanNumber, colIndex) => {
+                return(
+                    <div style={{display:"inline-block", marginBottom: -10}} >
+                    
+                        <div  key={colIndex}>
+                            <Button
+                            variant = "white"
+                            style = {{fontSize: "15px", width: 45, height: 45, borderColor: "lightgrey" , color: "black"}}
+                            value={this.state.InputButtonsB[rowIndex][colIndex]}
+                            onClick={this.UpdateButtonValues(rowIndex, colIndex, MatrixFlag)}
+                            > 
+                            {this.state.InputButtonsB[rowIndex][colIndex]}
+                            </Button>
+                        </div>
+                    </div>
+                );
+            });
+        }
     }
    
     HandleClick(e) 
@@ -130,20 +183,21 @@ class BooleanMatrices extends React.Component
         var ArrayA = this.state.InputButtonsA;
         var ArrayB = this.state.InputButtonsB;
 
-        alert(ArrayA)
-        alert(ArrayB)
-        //handle and set before here
+        this.setState(prevState => {
 
-        this.BooleanAnd(ArrayA, ArrayB)
-        this.BooleanOr(ArrayA, ArrayB)
-        this.BooleanProduct(ArrayA, ArrayB)
-        this.BooleanProduct(ArrayB, ArrayA)
+            return{
+                AnswerAnd: this.BooleanAnd(ArrayA, ArrayB),
+                AnswerOr: this.BooleanOr(ArrayA, ArrayB),
+                AnswerProduct: this.BooleanProduct(ArrayA, ArrayB),
+                AnswerReverseProduct: this.BooleanProduct(ArrayB, ArrayA)
+            };
+        });
 
     }
 
     BooleanAnd(ArrayA, ArrayB)
     {
-        alert("Entered Function And")
+        //alert("Entered Function And")
         var ArrayC = [];
 
         for(var i = 0; i < ArrayA.length; i++)
@@ -159,13 +213,13 @@ class BooleanMatrices extends React.Component
             }
             ArrayC.push(aRow)
         }
-        alert(ArrayC)
+        //alert(ArrayC)
         return ArrayC
     }
 
     BooleanOr(ArrayA, ArrayB)
     {
-        alert("Entered Function Or")
+        //alert("Entered Function Or")
         var ArrayC = [];
 
         for(var i = 0; i < ArrayA.length; i++)
@@ -181,13 +235,12 @@ class BooleanMatrices extends React.Component
             }
             ArrayC.push(aRow);
         }
-        alert(ArrayC);
+        //alert(ArrayC);
         return ArrayC;
     }
 
     BooleanProduct(Array1, Array2) //Handles finding the Boolean Product of two matricies
     {
-        alert("Entered Function Product");
         var ArrayC = [];
 
         var i, j = 0;
@@ -221,8 +274,83 @@ class BooleanMatrices extends React.Component
             ArrayC.push(aRow)
         }
         
-        alert("Array1 = " + Array1 + "    Array2 = " + Array2 +  ",    ArrayC = " + ArrayC);
+        //alert("Array1 = " + Array1 + "    Array2 = " + Array2 +  ",    ArrayC = " + ArrayC);
         return ArrayC;
+    }
+
+    displayAnswer()
+    {
+        if(this.state.AnswerAnd != undefined && this.state.AnswerOr != undefined && this.state.AnswerProduct != undefined && this.state.AnswerReverseProduct != undefined)
+        {
+            return(
+                <div style={{marginLeft: 30, width: "100%"}}>
+                    
+                    <div style={{float: "left", marginRight: "50px"}}>
+                   <b>A &and; B </b> 
+                        <Card style={{borderColor:"gray"}}> 
+                            {this.state.AnswerAnd.map((row, i) =>
+                            <div style={{padding: "0px",marginBottom: -5}} key={i}>
+                                {this.state.AnswerAnd[i].map((col, j) => 
+                                    <div style={{display:"inline-block", marginLeft: 5, marginRight: 5, padding: "5px"}}>
+                                        {this.state.AnswerAnd[i][j]}
+                                    </div>
+                                )}
+                            </div>
+                            )}
+                        </Card>
+                    </div>
+                    <div style={{float: "left", marginRight: "50px", marginBottom: "-15px"}}>
+                  <b> A &or; B </b> 
+                        <Card style={{borderColor:"gray"}}> 
+                            {this.state.AnswerOr.map((row, i) =>
+                                <div style={{padding: "0px",marginBottom: -5}} key={i}>
+                                    {this.state.AnswerOr[i].map((col, j) => 
+                                        <div style={{display:"inline-block", marginLeft: 5, marginRight: 5, padding: "5px"}}>
+                                            {this.state.AnswerOr[i][j]}
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                        </Card>
+                    </div>
+                    
+                    <div style={{float: "left", marginRight: "50px"}}>
+
+                   <b> A X B </b> 
+                        <Card style={{borderColor:"gray"}}> 
+                            {this.state.AnswerProduct.map((row, i) =>
+                                <div style={{padding: "0px",marginBottom: -5}} key={i}>
+                                    {this.state.AnswerProduct[i].map((col, j) => 
+                                        <div style={{display:"inline-block", marginLeft: 5, marginRight: 5, padding: "5px"}}>
+                                            {this.state.AnswerProduct[i][j]}
+                                          </div>
+                                    )}
+                                </div>
+                            )}
+                        </Card>
+                    </div>
+                    <div style={{float: "left", marginRight: "50px"}}>
+                    <b> B X A </b>
+                        <Card style={{borderColor:"gray"}}> 
+                              {this.state.AnswerReverseProduct.map((row, i) =>
+                                <div style={{padding: "0px",marginBottom: -5}} key={i}>
+                                    {this.state.AnswerReverseProduct[i].map((col, j) => 
+                                        <div style={{display:"inline-block", marginLeft: 5, marginRight: 5, padding: "5px"}}>
+                                            {this.state.AnswerReverseProduct[i][j]}
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                        </Card>
+                </div>
+                </div>
+            )
+        }
+        return (
+            <div>
+                No Answer
+            </div>
+        )
     }
 
     render()
@@ -258,26 +386,24 @@ class BooleanMatrices extends React.Component
                 
                      <Form.Group>
                         <p style = {{marginLeft: "20px", marginTop: "50px"}}>
-                            In the provided input boxes, please provide the data for MatrixA and MatrixB 
+                            In the provided input boxes, please provide the data for MatrixA and MatrixB. These Matrices follow the standard format for a Matrix.<br></br>
+                            Rows are along the vertical axis and Columns are along the horizontal. Once you have entered all available values for Matrix A and <br></br>
+                            Matrix B, click the submit button to get your answers below.
                         </p> <br></br>
-
-
 
                         <div style={{display: "inline-block", marginLeft: "145px"}} class ="left-element">
                             <div style = {{fontSize: "25px"}}>Matrix A</div>
                                 <Card style={{ borderColor: "black"}}>
-                                    {this.createMatrixAForm()}
+                                    {this.createMatrixForm(0)}
                                 </Card>
-                                
                         </div>
 
                         <div style={{display: "inline-block", float:"right", position:'absolute', marginLeft:"100px", marginRight: "10px"}} class ="right-element">
                             <div style = {{fontSize: "25px"}}>Matrix B</div>
 
                             <Card style={{ borderColor: "black"}}>
-                                {this.createMatrixAForm()}
+                                {this.createMatrixForm(1)}
                             </Card>
-
                         </div>
 
                      </Form.Group>
@@ -286,14 +412,22 @@ class BooleanMatrices extends React.Component
                                 type="button" 
                                 style={{marginLeft: 412, marginRight: 410, marginTop: 15}}
                                 onClick={this.HandleClick}>
-                                button
+                                Submit
+                                
                                 </button>
                         </div>
+
+                    <Form.Group controlId="truthTableBuilder.cardOutput">
+                    <Form.Label>Result</Form.Label>
+                    <Card body style={{ minHeight: "100px" }}>
+                        {this.displayAnswer()}
+                    </Card>
+                </Form.Group>
+
                 </Form>
             </Card.Body>
           </Card>
 
-          
           </div>
 
           <footer style={{marginTop:"20px"}}>
@@ -326,9 +460,7 @@ class BooleanMatrices extends React.Component
                   <h3>&copy; 2020 Wolfram Beta. All Rights Reserved.</h3>
               </p>
               </div>
-
             </div>
-
             </div>
           </footer>
           </div>
