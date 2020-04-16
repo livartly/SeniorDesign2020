2c2000afb625bc0552c681e1143fd9f3790bd775
Liv Weaver <livweaverdesign@gmail.com>
2020-04-16 15:52:06 -0500

    Fixed footer cutting off 1.2 output, UI updates, added 1.2 examples

--- client/src/components/problem-solvers/TreeProof/TreeProof.js
+++ client/src/components/problem-solvers/TreeProof/TreeProof.js
@@ -141,62 +141,77 @@ class TreeProof extends React.Component {
       <div>
 
         <div className="container" style={{ marginTop: "50px" }}>
-          <Form>
-            <h1>Propositional Logic</h1>
-            <Form.Group controlId="truthTableBuilder.instructions">
-              <Form.Label>Instructions</Form.Label>
-              <p>
-                Enter a formula of standard propositional, predicate, or modal logic. To enter logic symbols, use the buttons above the text field, or
-                  type in formula. The page will try to find either a countermodel or
-              a <a href="https://en.wikipedia.org/wiki/Method_of_analytic_tableau">tree
-              proof (a.k.a. semantic tableau)</a>. Any alphabetic character is allowed as a propositional constant, predicate,
-              individual constant, or variable. The character may be followed by digits as
-              indices. Predicates and function terms must be in prefix notation. The order of precedence among
-            connectives is <span class="formula">¬, ∧, ∨, →, ↔</span>. Association is to
-                the right.
-            </p>
-            </Form.Group>
-            <Form.Group controlId="truthTableBuilder.textInput">
-              <Form.Label>Insert Symbols</Form.Label>
-              <span class="formula">~</span> for <span class="formula">¬</span>,
-              <span class="formula">&</span> for <span class="formula">∧</span>,
-              <span class="formula">v</span> for <span class="formula">∨</span>,
-              <span class="formula">-&gt;</span> for <span class="formula">→</span>,
-              <span class="formula">&lt;-&gt;</span> for <span class="formula">↔</span>,
-            <form id="inputForm" action="." method="get" onsubmit="return false">
-                <div id="symbolButtonRow">
-                  <span class="hideOnTablet"></span>
-                  <div id="symbolButtons">
-                    <div class="symbutton button formula" onClick={this.insertAtKaret("¬")}>¬</div>
-                    <div class="symbutton button formula" onClick={this.insertAtKaret("∧")}>∧</div>
-                    <div class="symbutton button formula" onClick={this.insertAtKaret("∨")}>∨</div>
-                    <div class="symbutton button formula" onClick={this.insertAtKaret("→")}>→</div>
-                    <div class="symbutton button formula" onClick={this.insertAtKaret("↔")}>↔</div>
+          <Card>
+            <Card.Body>
+              <Form>
+              <h1>Propositional Logic</h1>
+              <Form.Group controlId="truthTableBuilder.instructions">
+                <Form.Label>Instructions</Form.Label>
+                <p>
+                  Enter a formula of standard propositional, predicate, or modal logic. To enter logic symbols, use the buttons above the text field, or
+                    type in formula. The page will try to find either a countermodel or
+                a <a href="https://en.wikipedia.org/wiki/Method_of_analytic_tableau">tree
+                proof (a.k.a. semantic tableau)</a>. Any alphabetic character is allowed as a propositional constant, predicate,
+                individual constant, or variable. The character may be followed by digits as
+                indices. Predicates and function terms must be in prefix notation. The order of precedence among
+              connectives is <span class="formula">¬, ∧, ∨, →, ↔</span>. Association is to
+                  the right.
+              </p>
+              </Form.Group>
+              <Form.Group controlId="treeProof.examples">
+                  <Form.Label>Examples</Form.Label>
+                  <ul>
+                    <li>
+                    A ∧ (B→C)→(B→(A∧C))
+                    </li>
+                    <li>
+                    (A ∧ B) →¬(A ∧ ¬B)
+                    </li>
+                  </ul>
+              </Form.Group>
+              <Form.Group controlId="treeProof.textInput">
+                <Form.Label>Insert Symbols</Form.Label>
+                <span class="formula">~</span> for <span class="formula">¬</span>,
+                <span class="formula">&</span> for <span class="formula">∧</span>,
+                <span class="formula">v</span> for <span class="formula">∨</span>,
+                <span class="formula">-&gt;</span> for <span class="formula">→</span>,
+                <span class="formula">&lt;-&gt;</span> for <span class="formula">↔</span>,
+              <form id="inputForm" action="." method="get" onsubmit="return false">
+                  <div id="symbolButtonRow">
+                    <span class="hideOnTablet"></span>
+                    <div id="symbolButtons">
+                      <div class="symbutton button formula" onClick={this.insertAtKaret("¬")}>¬</div>
+                      <div class="symbutton button formula" onClick={this.insertAtKaret("∧")}>∧</div>
+                      <div class="symbutton button formula" onClick={this.insertAtKaret("∨")}>∨</div>
+                      <div class="symbutton button formula" onClick={this.insertAtKaret("→")}>→</div>
+                      <div class="symbutton button formula" onClick={this.insertAtKaret("↔")}>↔</div>
+                    </div>
                   </div>
-                </div>
-                <div id="proveRow">
-                  <input
-                    type="text"
-                    size="60"
-                    name="flaField"
-                    id="flaField"
-                    class="formula"
-                    onChange={this.updateInput}
-                    value={this.state.inputText}>
-                  </input>
-                  <input value="Submit" id="proveButton" class="button" onClick={this.handleClick}></input>
-                </div>
-              </form>
-            </Form.Group>
-            <Form.Group controlId="truthTableBuilder.cardOutput">
-              <Form.Label>Result</Form.Label>
-            {/* For the output: */}
-            <Card>
-              <div id="model"> </div>
-              <div id="rootAnchor"> </div>
-            </Card>
-            </Form.Group>
-          </Form>
+                  <div id="proveRow">
+                    <input
+                      type="text"
+                      size="60"
+                      name="flaField"
+                      id="flaField"
+                      class="formula"
+                      onChange={this.updateInput}
+                      value={this.state.inputText}>
+                    </input>
+                    <input value="Submit" id="proveButton" class="button" onClick={this.handleClick}></input>
+                  </div>
+                </form>
+              </Form.Group>
+              <Form.Group controlId="treeProof.cardOutput">
+                <Form.Label>Result</Form.Label>
+                <Card body style={{ minHeight: "400px" }}>
+                <div id="model"> </div>
+                <div id="rootAnchor"> </div>
+              </Card>
+              </Form.Group>
+            </Form>
+            </Card.Body>
+          </Card>
+          <br></br>
         </div>
       </div>
     );
